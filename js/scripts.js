5772e375b58f76c45eab939e18000ea4724e2ca5
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-08 15:09:25 +0300

    working hover effect on the portifolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-$(document).ready(function() {
-    $("#design").click(function(){
-      $("#hideDesign").toggle();
+$(document).ready(function () {
+        $("#design").click(function () {
+            $("#hideDesign").toggle();
+        })
+        $("#development").click(function () {
+            $("#hideDevelopment").toggle();
+        })
+        $("#productManagement").click(function () {
+            $("#productManagementt").toggle();
+        })
     })
-    $("#development").click(function(){
-      $("#hideDevelopment").toggle();
-    })
-    $("#productManagement").click(function(){
-      $("#productManagementt").toggle();
-    })
-  })
-  
-  .hide;{
-    display:none;
-  }
-  
+
+    .hide; {
+    display: none;
+}
+$(".PORTIFOLIO").hover(function () {
+    $(this).children(".card-1").fadeToggle(1000, "linear");
+  });
