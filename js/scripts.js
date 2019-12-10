48f25fd53f7e7174c3901b1dcd4ca7cec9796ac3
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-10 11:05:41 +0300

    updated toggle and pop up notification

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,21 @@ $(document).ready(function () {
         $("#productManagement").click(function () {
             $("#productManagementt").toggle();
         })
+        $('#mc-embedded-subscribe').click(function () {
+            var name = document.getElementById("mce-NAME").value;
+            alert("Hey " + name + ". We have received your details thank you for reaching us!");
+        });
+        $(".card-1").hover(function(){
+            $(this).animate({opacity:'1'});
+        },
+        function(){4
+            $(this).animate({opacity:'0'});
+            });
+        // $(".card").hover(function () {
+        //     $(document).ready(".card-1").fadeToggle(1000, "linear");
+        //   });
+        
+        
     })
 
     .hide; {
